4b62fb8a6a8b9683cba613c176177f476cfd7d36
abhijithjithu0007 <abhijithabhi36151@gmail.com>
2024-09-27 09:47:24 +0530

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+import Link from "next/link"
+
+export default function page() {
+    return (
+        <>
+            <h1>fefefef</h1>
+            <Link href='/login'>CLick here to login</Link>
+        </>
+
+    )
+}
